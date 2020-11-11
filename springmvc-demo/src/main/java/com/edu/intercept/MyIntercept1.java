dcbf4d3b08f32391345f3477b10357a7c3bba632
zero <272880214@qq.com>
2020-11-11 23:24:53 +0800

    spring-mvc

---
+++ springmvc-demo/src/main/java/com/edu/intercept/MyIntercept1.java
@@ -0,0 +1,26 @@
+package com.edu.intercept;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyIntercept1 implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // true是放行  false是终止
+        System.out.println("MyIntercept1-------------preHandle-------");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyIntercept1-------------postHandle-------");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("MyIntercept1-------------afterCompletion-------");
+    }
+}
