cd72d0cc38d5d680f1dab6292c858b75c1100e3c
fuwei <272880214@qq.com>
2020-11-16 18:22:03 +0800

    mvc

---
+++ mvc/src/main/java/com/adt/edu/mvcframework/pojo/Handler.java
@@ -0,0 +1,53 @@
+package com.adt.edu.mvcframework.pojo;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Handler {
+    private Object controller; // method.(invokeobj,)
+    private Method method;
+    private Pattern pattern; // spring中url是⽀持正则的
+    private Map<String, Integer> paramIndexMapping; // 参数顺序,是为了进⾏参数绑定，key是参数名，value代表是第⼏个参数 <name,2>
+
+    public Handler(Object controller, Method method, Pattern pattern) {
+        this.controller = controller;
+        this.method = method;
+        this.pattern = pattern;
+        this.paramIndexMapping = new HashMap<>();
+    }
+
+    public Object getController() {
+        return controller;
+    }
+
+    public void setController(Object controller) {
+        this.controller = controller;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public Map<String, Integer> getParamIndexMapping() {
+        return paramIndexMapping;
+    }
+
+    public void setParamIndexMapping(Map<String, Integer>
+                                             paramIndexMapping) {
+        this.paramIndexMapping = paramIndexMapping;
+    }
+}
