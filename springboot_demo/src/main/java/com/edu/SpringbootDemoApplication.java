c02df38cf9f3feaa421a9c05c31beb7e4530a0fb
zero <272880214@qq.com>
2020-12-10 23:25:16 +0800

    jdk动态代理

--- springboot_demo/src/main/java/com/edu/SpringbootDemoApplication.java
+++ springboot_demo/src/main/java/com/edu/SpringbootDemoApplication.java
@@ -3,7 +3,7 @@ package com.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//扫描组件并自动配置springboot
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
