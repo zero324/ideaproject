18d6908158f053fd90f56065b1857353c83b9f27
zero <272880214@qq.com>
2020-12-16 22:09:36 +0800

    springboot-thymeleaf

--- springboot_data/src/main/java/com/edu/mapper/CommentMapper.java
+++ springboot_data/src/main/java/com/edu/mapper/CommentMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper// 如果mapper注解过多用在启动类上加?@MapperScan("xxx")
+//mapper是标识的作用mybtis接口文件   并且让springboot能够扫描生成mapper代理对象 存到容器中
 public interface CommentMapper {
 
     @Select("SELECT * FROM t_comment WHERE id =#{id}")
